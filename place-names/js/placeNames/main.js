3b030dbea8570227578feaa62fde43b6990c5c77
Tristan Wyatt <69435758+tristan0508@users.noreply.github.com>
2020-11-13 15:15:45 -0500

    converted

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,17 @@
-function main() {
-  // Put your code here
+
+const main = () => {
+
+  const names = [ "Nashville", "Hong Kong", "The back yard", "Earth",
+  "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+  console.log("All Place Names")
+
+  names.map(name => console.log(name))
+  
+  console.log("")
+
+  console.log("'The' Place Names")
+  const a = names.filter(a => a.startsWith("The"))
+  a.map(name => console.log(name))
 
 }
 
