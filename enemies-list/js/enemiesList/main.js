3deb8fc057a09f7cd48c43c58221eac4befa0ca0
Tristan Wyatt <69435758+tristan0508@users.noreply.github.com>
2020-11-13 16:19:31 -0500

    refactored c# to js

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,5 +1,41 @@
 function main() {
-  // Put your code here
+  console.log("My Enemies List!")
+  console.log("----------------")
+
+  const enemies = [
+    {
+      FirstName: "Joshua",
+      LastName: "Flowers",
+      Offenses: ["Being a jerk to me in elemntary school","Not being nice to me in elemntary school"],
+      isReallyHated: true
+    },
+    {
+      FirstName: "Darth",
+      LastName: "Vader",
+      Offenses: ["Cut off Luke's hand","Murdered all those kids", "Unkind management practices"],
+      isReallyHated: false
+    },
+    {
+      FirstName: "Betty",
+      LastName: "Rudelady",
+      Offenses: ["Phone calls in the theater","Phone calls on the bus","Phone calls in line at the grocery store","Poor conversationalist"],
+      isReallyHated: true
+    },
+    {
+      FirstName: "Leon",
+      LastName: "Peck",
+      Offenses: ["Being a jerk to me in elemntary school","Not being nice to me in elemntary school"],
+      isReallyHated: false
+    },
+
+  ]
+  enemies.map(enemy => {
+    if(enemy.isReallyHated){
+      return console.log(`${enemy.FirstName} ${enemy.LastName} Really, really dislike!`)
+    } else {
+      return console.log(`${enemy.FirstName} ${enemy.LastName}`)
+    }
+  })
 
 }
 
