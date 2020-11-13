89f61786ea69385ee44d23d3dd42c8a64971d83f
Tristan Wyatt <69435758+tristan0508@users.noreply.github.com>
2020-11-13 17:02:37 -0500

    dies are being added and return doubles

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,5 +1,30 @@
 function main() {
-  // Put your code here
+  console.log("Let's roll some dice, baby!")
+  console.log("---------------------------")
+
+  const roll = () => {
+    const randomNum = Math.floor(Math.random() * 3) + 1
+
+    return {
+      value: randomNum
+    }
+  }
+
+  for (let i = 0; i< 10; i++){
+   
+
+    const die1 = roll();
+    const die2 = roll();
+    let message = `? + ? = ${die1.value + die1.value}`
+
+    if (die1.value === die2.value){
+      message += " DOUBLES!"
+    }
+
+    console.log(message)
+
+    
+  }
 
 }
 
